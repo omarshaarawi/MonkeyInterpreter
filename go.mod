c9b3f025a6feb600ae53aa0f6f4f8c426ee93928
Omar <omar25shaarawi@gmail.com>
2023-05-14 20:11:10 -0500

    initial token and lexer complete - 1.4

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/omarshaarawi/monkey
+
+go 1.20
---
+++ token/token.go
@@ -0,0 +1,66 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	LT = "<"
+	GT = ">"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPARAN  = "("
+	RPARAN  = ")"
+	LBRRACE = "{"
+	RBRRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
